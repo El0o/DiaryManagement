c9861ee0010e13a2245d4f66cdc128f4b22b1954
Elise M <elise.morana@efrei.net>
2023-12-04 18:20:16 +0100

    Which level and Insert 1 functions + corrections

--- Part_1-2/copy_main_1.c
+++ Part_1-2/copy_main_1.c
@@ -3,8 +3,8 @@
 
 lvl_list *test = create_empty_list(5); // Creates a level list of 5 levels, from 0 to 4
 
-    add_head(test, 4, 1); //Add an head on the level n
-    insert_cell(test, 2, 42); //Insert a cell on the nth level
+    add_head(test, 4, 1); //Add a head on the level n
+    insert_cell(test, 2, 42); //Insert a cell on the nth level and all the lower ones
 
 
     display_list(*test);
